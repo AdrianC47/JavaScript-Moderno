e3f7fc6033b878047d5280da58ddb3f7aed500a3
Adrian <luiscabreraucme@hotmail.com>
2022-02-14 18:48:38 -0500

    Introduccion a variables y comentarios

--- 01-Fundamentos/app.js
+++ 01-Fundamentos/app.js
@@ -1,5 +1,17 @@
 // console.log(console.log("Hello friend"));
 
-let a = 10;
-var b = 10;
-const c = 10;
+//let a = 10;//Ambas variables let y var
+//var b = 10; //sirven para decirle a Javascript que estoy creando una nueva variable 
+//const c = 10;// la diferencia es que no voy a poder cambiar el valor de una variable const
+
+//var es una forma antigua de inicializar una variable
+
+
+//c = 20; //esto no se puede hacer
+// d = 40; esto es mala practica siempre debo inicializar mi variable
+//a = 20;
+//b = 30;
+
+
+let a = 10, b = 10, c = 10, d = 10, x = a + b;
+console.log(x)
