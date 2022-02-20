2419cda1926847edc2968e7735c1a4edee4de140
Adrian <luiscabreraucme@hotmail.com>
2022-02-20 12:37:50 -0500

    Introduccion general a los tipos primitivos

---
+++ 01-Fundamentos/assets/js/primitivos.js
@@ -0,0 +1,35 @@
+
+let nombre = 'Peter Parker'; // esto es un String
+console.log(nombre);
+
+nombre = 'Ben Parker';
+console.log(nombre);
+
+nombre = "Tía May"; //esta y 
+nombre = `Tía May`; //esta son otras formas de usar strings
+console.log(typeof nombre);
+
+nombre = 123;
+console.log(typeof nombre);
+
+let esMarvel = false;
+console.log(typeof esMarvel);
+
+let edad = 33;
+console.log(typeof edad);
+
+edad = 33.001;
+console.log(typeof edad);
+
+let superPoder;
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(typeof symbol1);
+
+console.log(symbol1 === symbol2);
