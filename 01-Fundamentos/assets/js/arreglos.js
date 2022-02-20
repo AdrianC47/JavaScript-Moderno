cac90e4dd660a3b11bfd6137a00226fe0e609741
Adrian <luiscabreraucme@hotmail.com>
2022-02-20 18:53:34 -0500

    Arreglos

---
+++ 01-Fundamentos/assets/js/arreglos.js
@@ -0,0 +1,28 @@
+
+
+//const arr = new Array(10); //con esto creo un arreglo vacio con 10 casillas
+//const arr = []; //con esto creo un arreglo vacio
+//console.log(arr);
+
+let videojuegos = [ 'Zelda', 'Mario', 'FIFA'];
+console.log({ videojuegos });
+
+console.log(videojuegos[0]);
+
+let arregloCosas =[
+    true,
+    123,
+    'Adrian',
+    1 + 2,
+    function(){},
+    ()=>{},
+    { a: 1 },
+    ['X', 'Megaman', 'Zero', 'Dr.Light',[
+        'Dr. Willy',
+        'Woodman'
+    ]]
+];
+
+//console.log({arregloCosas});
+//console.log(arregloCosas[2]);
+console.log(arregloCosas[7][4][1]);
