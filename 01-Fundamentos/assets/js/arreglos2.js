09e48f2b821c315974ebd4e13b26c550150da9e0
Adrian <luiscabreraucme@hotmail.com>
2022-02-20 21:24:21 -0500

    Detalles sobre arreglos

---
+++ 01-Fundamentos/assets/js/arreglos2.js
@@ -0,0 +1,43 @@
+let juegos = ['Zelda', 'Mario', 'Metroid', 'Chrono'];
+console.log('Largo:', juegos.length );
+
+let primero = juegos[2-2];
+let ultimo = juegos[juegos.length-1];
+
+console.log('Primero: ', primero);
+console.log('Ultimo: ', ultimo);
+
+//Si quiero recorrer todo mi arreglo puedo usar un foreach
+
+juegos.forEach( (elemento, indice, arreglo) => {
+    console.log(elemento,indice,arreglo);
+} ); 
+
+let nuevaLongitud = juegos.push('F-Zero'); //añado un nuevo elemento al final del arrego
+console.log({ nuevaLongitud, juegos });
+
+nuevaLongitud = juegos.unshift('Fire Emblem'); //Añado un nuevo elemento al inicio del arreglo
+console.log({ nuevaLongitud, juegos}); 
+
+//Ahora digamos que quiero borrar un elemento que se encuentra al final
+
+let juegoBorrado= juegos.pop('F-Zero');
+console.log({juegoBorrado, juegos});
+
+//Ahora si quiero borrar un elemento en una posicion específica
+
+let pos = 1;
+console.log(juegos); 
+let juegosBorrados= juegos.splice(pos, 2);
+console.log({juegosBorrados, juegos});
+
+let metroidIndex = juegos.indexOf('Metroid'); //Es CaseSensitive, con el indexof busco el indice de un elemento de un arreglo
+console.log({metroidIndex})
+
+// TODO: Referencia
+
+
+
+
+
+
