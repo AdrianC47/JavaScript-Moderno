2ef43d50c59cccde0d2ee873479a3ec197ccc15f
Adrian <luiscabreraucme@hotmail.com>
2022-03-13 00:39:13 -0500

    Recordatorio diferencia en el Uso de Let y Var

--- 01-Fundamentos/assets/js/bases/app.js
+++ 01-Fundamentos/assets/js/bases/app.js
@@ -13,7 +13,10 @@ alert('Hola desde app.js');
 //a = 20;
 //b = 30;
 
-
+/*
+Let te permite declarar variables limitando su alcance (scope) al bloque, declaración o expresión donde se está usando.
+A diferencia de la palabra clave var la cual define una variable global o local en una función sin importar el ámbito
+del bloque*/ 
 let a = 10, 
     b = 20, 
     c = 'Hola ', 
